846909e34ff28a2a03547818aefb32479da132b0
a1ecsss <estebana.delapena@gmail.com>
2025-03-13 19:36:35 -0600

    first commit

---
+++ Pokemon.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class Pokemon {
+    private String name;
+    private String type1;
+    private String type2;
+    private List<String> abilities;
+
+    public Pokemon(String name, String type1, String type2, List<String> abilities) {
+        this.name = name;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.abilities = abilities;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public List<String> getAbilities() {
+        return abilities;
+    }
+}
